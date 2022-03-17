5772749c81203281b410ece641029eedd5238619
mahmoudAllm <mahmoud.allam@paywithboost.com>
2022-03-17 15:41:14 +0200

    update cors

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+  allowedHeaders: "*",
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
